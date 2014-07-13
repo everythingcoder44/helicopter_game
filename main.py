ed161e36a938d220c27d87fc26ab931ddb5edd03
Ritik Mishra <ritik.mishra314@gmail.com>
2014-07-13 13:52:10 -0500

    Made the helicopter a class and made all the tabs spaces

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-import pygame, os
+#import modules
+import pygame
+import os
 import random
+
+#create the screen
 height = 500
 width = 1000
-y = 50
-change = 1
-color = [0, 0, 0]
-color_change = [1, 1, 1, 1]
 dimensions = (width, height)
-direct = [0.5, 0.5, 1, 1]
-screen = pygame.display.set_mode(dimensions)
+window = pygame.display.set_mode(dimensions)
 running = 1
+
+class Helicopter:
+    def __init__(self, screen):
+        self.change = 1
+        self.y = 50
+        self.img = pygame.image.load(os.path.join("resources", "Helicopter.png"))
+        self.click = 0
+        self.screen = screen
+    def move(self):
+        self.click = pygame.mouse.get_pressed()[0]
+        if self.click == 1:
+            self.change = -1
+        else:
+            self.change = 1
+        self.y += self.change
+    def blit(self):
+        self.screen.blit(self.img, (25, self.y))
+heli = Helicopter(window)
+
 while running:
-	event = pygame.event.poll()
-	if event.type == pygame.QUIT:
-		running = 0
-	screen.fill((0, 0, 0))
-	img = pygame.image.load(os.path.join("resources", "Helicopter.png"))
-	screen.blit(img, (25,y))
-	y += change
-	x = pygame.mouse.get_pressed()[0]
-	
-	if y == 5 or y == height-5:
-		raise Exception("You lost :( ")
-	if x == 1:
-		change = -1
-	else:
-		change = 1
-	
-	pygame.display.flip()
-	
-	
+    #makes the close button functional
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        running = 0
+    
+    #makes the background black 
+    window.fill((0, 0, 0))
+    
+    heli.move()
+    heli.blit()
+
+    pygame.display.flip()
+    
+    
